ebbb9fb96d2cf0ae38125f1ea45f7b9cb6c65ea7
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2026-05-29 15:32:30 +0200

    Creata base dell'applicazione express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("CMS-TS")
+import express, { type Request, type Response } from 'express'
+const app = express();
+const port: number = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  return res.send("cms-ts endpoint");
+});
+
+app.listen(port, () => {
+  console.log(`Applicazione in ascolto sulla porta ${port}`);
+});
