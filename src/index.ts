3eb1478d52bd21ed998b96b717b9f262856cca47
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2026-05-29 16:23:22 +0200

    Creata route base per ottenere il contenuto dell'articolo

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ const WP_BASE_URL = "http://cms.test/wp-json/wp/v2";
 
 app.use(express.json());
 
-const cmsV1Router = Router();
+const cmsRouter = Router();
 
 // Route per verificare la salute dell'API
-cmsV1Router.get('/', (req: Request, res: Response) => {
+cmsRouter.get('/', async (req: Request, res: Response) => {
   return res.json({
     name: 'cms-ts endpoint',
     success: true
@@ -17,7 +17,7 @@ cmsV1Router.get('/', (req: Request, res: Response) => {
 });
 
 // Route per ottenere i post dentro wordpress
-cmsV1Router.get('/posts', async (req: Request, res: Response) => {
+cmsRouter.get('/posts', async (req: Request, res: Response) => {
   try {
     // Effettua un fetch verso WordPress per recuperare tutti i post
     const response = await fetch(`${WP_BASE_URL}/posts`);
@@ -36,7 +36,41 @@ cmsV1Router.get('/posts', async (req: Request, res: Response) => {
   }
 });
 
-app.use(baseURL, cmsV1Router);
+type PostType = {
+  title: string;
+  content: string;
+  status: string;
+}
+
+// Route per inserire in wordpress l'articolo
+cmsRouter.post('/posts', async (req: Request, res: Response) => {
+  const authHeader = req.headers.authorization;
+  
+  // Verifica se l'autenticazione esiste o meno
+  if (!authHeader) {
+    return res.json({
+      success: false,
+      error: "Autenticazione mancante"
+    }).sendStatus(401);
+  };
+
+  // Verifica se il contenuto dell'articolo e' stato ricevuto
+  const postBodyData: PostType = req.body;
+  if (!postBodyData) {
+    return res.json({
+      success: false,
+      error: "La richiesta non e' stata effettuata correttamente"
+    }).sendStatus(400);
+  }
+
+  console.log(postBodyData.content)
+  console.log(postBodyData.title)
+  console.log(postBodyData.status)
+
+  return res.sendStatus(200);
+});
+
+app.use(baseURL, cmsRouter);
 
 app.listen(port, () => {
   console.log(`Applicazione in ascolto sulla porta ${port} e endpoint ${baseURL}`);
