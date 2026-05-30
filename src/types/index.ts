af710a5c51831f8b1604e00dc5b965c78d6f08c0
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2026-05-30 15:59:27 +0200

    Creato schema zod per validazione dati

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,12 @@ export const PostSchema = z.object({
 });
 
 export type PostType = z.infer<typeof PostSchema>;
+
+export const UserSchema = z.object({
+  name: z.string("Il nome e' obbligatorio").min(5),
+  email: z.email("L'email e' obbligatoria").toLowerCase(),
+  password: z.string("La password e' obbglitatoria").min(8),
+  wp_app_password: z.string("API Password necessaria").min(24)
+});
+
+export type UserType = z.infer<typeof UserSchema>;
