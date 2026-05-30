3edba99837fe555e6fa6cce3ea600786d34f81e8
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2026-05-30 17:05:11 +0200

    Aggiunto fetch per creare l'utente

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import { UserSchema, type UserType } from '@/types';
 import type { Request, Response } from 'express';
 import { handleError } from '@/routes/errors';
+import { config } from '@/lib';
 
 export const createUser = async (req: Request, res: Response) => {
   const userData: UserType = req.body;
@@ -19,9 +20,32 @@ export const createUser = async (req: Request, res: Response) => {
       password: userData.password
     });
 
-    console.log(parseUserData.name)
-    console.log(parseUserData.email)
-    console.log(parseUserData.password)
+    // Autenticazione <nome-utente>:<API password> in bae64
+    const auth64 = btoa(`${config.wp.adminUsername}:${config.wp.adminPassword}`);
+
+    const response = await fetch(`${config.wp.baseUrl}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${auth64}`
+      },
+      body: JSON.stringify({
+        username: userData.name,
+        email: userData.email,
+        password: userData.password,
+        roles: ['author']
+      }),
+      tls: {
+        rejectUnauthorized: false // ignora certificato self-signed in locale
+      }
+    })
+
+    console.log(await response.json())
+    if (response.status == 200) {
+      const wpUser = await response.json();
+      console.log(wpUser);
+    }
+
 
   } catch (error) {
     return handleError(error, res);
