1bed0238f90674e47a7200a1e2e8dbc7243d8a11
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2026-05-31 16:50:51 +0200

    Aggiornato commento

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,7 +3,7 @@ import type { JWTPayload } from 'jose';
 // Serve a modificare un tipo che esiste gia' globalmente
 declare global {
   namespace Express {
-    // Ad Express aggiunge a Request user di JWTPayload
+    // Ad Express.Request viene aggiunto user dell'interfaccia JWTPayload
     interface Request {
       user?: JWTPayload; // user potrebbe non esistere
     }
