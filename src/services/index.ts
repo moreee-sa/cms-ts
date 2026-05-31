7585c70600750240c5edc7e000ce66c3784cab26
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2026-05-31 18:28:21 +0200

    Aggiunte istruzioni del modello gemini

--- src/services/index.ts
+++ src/services/index.ts
@@ -13,6 +13,15 @@ export const getAISuggestion  = async (title: string, content: string) => {
 
   const response = await ai.models.generateContent({
     model: 'gemini-2.5-flash',
+    // Questa configurazione si danno delle istruzioni precise a Gemini
+      config: {
+        systemInstruction: `Sei un editor giornalistico italiano. 
+          Migliora SOLO il contenuto del testo che ti viene fornito, senza modificare il titolo.
+          Rispondi ESCLUSIVAMENTE con il contenuto migliorato, senza commenti, spiegazioni, introduzioni o note.
+          Non aggiungere titoli, intestazioni o sezioni extra.
+          Non spiegare cosa hai fatto o perché.
+          Solo il contenuto migliorato, nient'altro.`
+      },
     contents: `Titolo: ${title}\n\nContenuto: ${content}`,
   });
 
